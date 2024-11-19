cd0e241f9a4bdf66318023bcb0957c6e531d2fc2
Lokesh2003GN <145318691+Lokesh2003GN@users.noreply.github.com>
2024-11-19 23:30:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,6 +39,5 @@ def get_users():
 
     return jsonify(users)
 
-if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
